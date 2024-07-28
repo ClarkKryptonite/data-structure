19fdf8a0d7337aeeb2ab288cc57b2263807829b3
ClarkKryptonite <2742649263@qq.com>
2024-07-29 01:11:17 +0800

    Modify(SortTest): optimize code

--- src/main/java/com/example/datastructure/algorithm/sort/assist/SortExample.java
+++ src/main/java/com/example/datastructure/algorithm/sort/assist/SortExample.java
@@ -1,43 +1,37 @@
 package com.example.datastructure.algorithm.sort.assist;
 
+import com.example.datastructure.util.MeasureUtils;
 import com.example.datastructure.util.NumberUtils;
 
 import java.util.Arrays;
+import java.util.function.BiConsumer;
 
 public class SortExample {
-    private final int[] randomArr = NumberUtils.generateRandomNumbers();
-    private final int[] randomDuplicateArr = NumberUtils.generateDuplicateNumbers();
-    private final int[] randomUniqueArr = NumberUtils.generateUniqueRandomNumbers();
+    private final int[][] testCaseArr = new int[][]{
+            NumberUtils.generateRandomNumbers(),
+            NumberUtils.generateDuplicateNumbers(),
+            NumberUtils.generateUniqueRandomNumbers()
+    };
 
     public SortExample() {
         System.out.println("--- SortExample ---");
-        System.out.println("randomArr:" + Arrays.toString(randomArr));
-        System.out.println("randomDuplicateArr:" + Arrays.toString(randomDuplicateArr));
-        System.out.println("randomUniqueArr:" + Arrays.toString(randomUniqueArr));
+        for (int i = 0; i < testCaseArr.length; i++) {
+            System.out.println("testCase " + i + ":" + Arrays.toString(testCaseArr[i]));
+        }
         System.out.println();
     }
 
-    private int[] copyRandomArr() {
-        return randomArr.clone();
-    }
-
-    private int[] copyRandomDuplicateArr() {
-        return randomDuplicateArr.clone();
-    }
-
-    private int[] copyRandomUniqueArr() {
-        return randomUniqueArr.clone();
-    }
-
-    public void printSort(ISort sort) {
+    public void printSort(ISort sort, BiConsumer<int[], int[]> callback) {
         System.out.println("--- SortName:" + sort.getClass().getSimpleName() + " ---");
-        printDifferentArr(sort, "RandomArr", copyRandomArr());
-        printDifferentArr(sort, "DuplicateArr", copyRandomDuplicateArr());
-        printDifferentArr(sort, "UniqueArr", copyRandomUniqueArr());
+        for (int[] testCase : testCaseArr) {
+            printDifferentArr(sort, testCase, callback);
+        }
     }
 
-    private void printDifferentArr(ISort sort, String name, int[] arr) {
-        sort.sort(arr);
-        System.out.println("Sort " + name + ":" + Arrays.toString(arr));
+    private void printDifferentArr(ISort sort, int[] arr, BiConsumer<int[], int[]> callback) {
+        int[] cloneArr = arr.clone();
+        MeasureUtils.measureExecutionTime(() -> sort.sort(cloneArr));
+        System.out.println("sort result:" + Arrays.toString(cloneArr));
+        callback.accept(arr.clone(), cloneArr);
     }
 }
--- src/test/java/com/example/datastructure/sort/SortTest.java
+++ src/test/java/com/example/datastructure/sort/SortTest.java
@@ -4,22 +4,30 @@ import com.example.datastructure.algorithm.sort.InsertSort;
 import com.example.datastructure.algorithm.sort.QuickSort;
 import com.example.datastructure.algorithm.sort.assist.ISort;
 import com.example.datastructure.algorithm.sort.assist.SortExample;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.function.BiConsumer;
 
 public class SortTest {
 
     private static final SortExample example = new SortExample();
+    private static final BiConsumer<int[], int[]> callback = (arr, sortedArr) -> {
+        Arrays.sort(arr);
+        Assertions.assertArrayEquals(arr, sortedArr);
+    };
 
     @Test
     void quickSort() {
         ISort sort = new QuickSort();
-        example.printSort(sort);
+        example.printSort(sort, callback);
     }
 
     @Test
     void insertSort() {
         ISort sort = new InsertSort();
-        example.printSort(sort);
+        example.printSort(sort, callback);
     }
 
 }
